9cb0216f182d898456ff9efebd9f6553728dad9a
Gregor <43413852+Albadylic@users.noreply.github.com>
2024-11-27 18:11:10 +0000

    style the page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,18 @@
 import React, { useState } from "react";
 
+import { ReactComponent as Shrek } from "./shrek.svg";
+
+{
+  /* <a target="_blank" href="https://icons8.com/icon/26BhkJZRzRCR/shrek">Shrek</a> icon by <a target="_blank" href="https://icons8.com">Icons8</a> */
+}
+
 function App() {
   const [question, setQuestion] = useState("");
   const [response, setResponse] = useState("");
+  const [loading, setLoading] = useState(false);
 
   const handleSubmit = async (e) => {
+    setLoading(true);
     e.preventDefault();
     try {
       const res = await fetch("http://127.0.0.1:8000/ask", {
@@ -20,23 +28,38 @@ function App() {
       console.error("Error:", error);
       setResponse("An error occurred.");
     }
+    setLoading(false);
   };
 
   return (
-    <div className="App">
-      <h1>Ask Shrek</h1>
-      <form onSubmit={handleSubmit}>
-        <input
-          type="text"
-          value={question}
-          onChange={(e) => setQuestion(e.target.value)}
-          placeholder="Ask your question..."
-        />
-        <button type="submit">Submit</button>
-      </form>
-      <div>
-        <h2>Response:</h2>
-        <p>{response.response}</p>
+    <div className="App h-screen flex  flex-col items-center justify-center bg-lime-800">
+      <div className="flex flex-col items-center justify-center bg-stone-400/70	rounded p-5 my-2 min-w-96">
+        <h1 className="text-3xl font-semibold my-3">Ask Shrek</h1>
+        <form
+          onSubmit={handleSubmit}
+          className="text-l my-2 flex justify-center"
+        >
+          <textarea
+            value={question}
+            onChange={(e) => setQuestion(e.target.value)}
+            placeholder="Ask your question..."
+            className="p-2 mx-1 rounded"
+          />
+          <button
+            type="submit"
+            className="p-2 mx-1 bg-lime-950	text-white rounded max-h-32"
+            disabled={loading}
+          >
+            Submit
+          </button>
+        </form>
+      </div>
+      <div className="flex flex-col items-center justify-center bg-stone-400/70	rounded p-5 m-2">
+        {loading ? (
+          <Shrek className="animate-pulse" />
+        ) : (
+          <p>{response.response || "Response will generate here"}</p>
+        )}
       </div>
     </div>
   );
